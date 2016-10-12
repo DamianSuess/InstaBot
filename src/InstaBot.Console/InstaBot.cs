b51ec53c51a0db10fed94027d19fee0a6f2d0b5e
Arnaud Druffin <arnaud.druffin@gmail.com>
2016-10-12 12:50:47 -0400

    [develop] Add unhandled task exception log message

--- src/InstaBot.Console/InstaBot.cs
+++ src/InstaBot.Console/InstaBot.cs
@@ -1,4 +1,6 @@
-﻿using InstaBot.Console.Tasks;
+﻿using System.Threading.Tasks;
+using InstaBot.Console.Tasks;
+using InstaBot.Logging;
 
 namespace InstaBot.Console
 {
@@ -10,16 +12,24 @@ namespace InstaBot.Console
     public class InstaBot : IInstaBot
     {
         public ConfigurationManager ConfigurationManager { get; set; }
+        public ILogger Logger { get; set; }
         public ILogin LoginTask { get; set; }
         public ILikeTask LikeTask { get; set; }
         public IFollowingTask FollowingTask { get; set; }
-
+            
 
         public void Run()
         {
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
             LoginTask.DoLogin();
             LikeTask.Start();
             FollowingTask.Start();
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Logger.Critical("Fatal error occured in task", e.Exception);
         }
     }
 }
