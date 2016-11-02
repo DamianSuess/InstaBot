dc4ae2a3404d600ce5e2885aa4574bef0d6d376a
Arnaud Druffin <adruffin@cpaquebec.ca>
2016-11-02 13:09:48 -0400

    [develop] Catch unhandled exception

--- src/InstaBot.Console/Program.cs
+++ src/InstaBot.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Autofac;
 using InstaBot.Console.Utils;
 using InstaBot.Logging;
@@ -12,8 +13,22 @@ namespace InstaBot.Console
         //https://github.com/mgp25/Instagram-API/tree/master/src
         private static void Main(string[] args)
         {
-            Container = AutofacConfig.ConfigureContainer(args.Any() && !string.IsNullOrWhiteSpace(args[0]) ? args[0] : string.Empty);
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
 
+            Initalize(args);
+            Start();
+
+            System.Console.ReadLine();
+            Environment.Exit(0);
+        }
+        private static void Initalize(string[] args)
+        {
+            Container =
+                AutofacConfig.ConfigureContainer(args.Any() && !string.IsNullOrWhiteSpace(args[0]) ? args[0] : string.Empty);
+        }
+
+        private static void Start()
+        {
             using (var scope = Container.BeginLifetimeScope())
             {
                 var logger = scope.Resolve<ILogger>();
@@ -21,8 +36,30 @@ namespace InstaBot.Console
                 var bot = scope.Resolve<IInstaBot>();
                 bot.Run();
             }
+        }
 
+
+        private static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                using (var scope = Container.BeginLifetimeScope())
+                {
+                    var logger = scope.Resolve<ILogger>();
+                    var exception = e.ExceptionObject as Exception;
+                    if (exception != null)
+                        logger.Critical(exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+
+            System.Console.WriteLine(e.ExceptionObject.ToString());
+            System.Console.WriteLine("Press Enter to continue");
             System.Console.ReadLine();
+            Environment.Exit(1);
         }
     }
 }
